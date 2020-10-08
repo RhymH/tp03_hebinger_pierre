3a8418715ceba92d7a6d3c7ed9be9e61447a46a9
Pierre Hebinger <hebinger.68@gmail.com>
2020-10-08 16:59:07 +0200

    Ajout bouchon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { FooterComponent } from './footer/footer.component';
+import { TetiereComponent } from './tetiere/tetiere.component';
+import { TableComponent } from './catalogue/table/table.component';
+import { RechercheComponent } from './catalogue/recherche/recherche.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TetiereComponent,
+    CatalogueComponent,
+    TableComponent,
+    RechercheComponent
   ],
   imports: [
     BrowserModule
